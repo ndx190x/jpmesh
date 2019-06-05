d793f6d9c6ad71c0e0981b6fd25b7f966db338d0
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-06-05 17:39:41 +0900

    Convert to 0.1km mesh

--- R/converter.R
+++ R/converter.R
@@ -1,17 +1,15 @@
-#' @param to_mesh_size target mesh type. From 80km to 0.125km. If `NULL`,
+#' @param to_mesh_size target mesh type. From 80km to 0.100km. If `NULL`,
 #' the meshcode of one small scale will be returned.
+#' @return character.
 mesh_convert <-
   function(meshcode = NULL, to_mesh_size = NULL, scale_down = TRUE, ...) { # nolint
   if (is.mesh(meshcode))
     if (rlang::is_null(to_mesh_size))
-      if (rlang::is_true(scale_down))
-        to_mesh_size <-
-          units::drop_units(df_mesh_size_unit$mesh_size[which(mesh_units == mesh_size(meshcode)) + 1]) # nolint
-      else
-        to_mesh_size <-
-          units::drop_units(df_mesh_size_unit$mesh_size[which(mesh_units == mesh_size(meshcode)) - 1]) # nolint
+      to_mesh_size <- ifelse(rlang::is_true(scale_down),
+                             units::drop_units(df_mesh_size_unit$mesh_size[which(mesh_units == mesh_size(meshcode)) + 1]), # nolint
+                             units::drop_units(df_mesh_size_unit$mesh_size[which(mesh_units == mesh_size(meshcode)) - 1])) # nolint
       to_mesh_size <- units::as_units(to_mesh_size, "km")
-      if (mesh_size(meshcode) > to_mesh_size) {
+      if (min(mesh_size(meshcode)) > to_mesh_size) {
         if (to_mesh_size == units::as_units(10, "km"))
           res <- grep(
             pattern = paste0("^(", meshcode, ")"),
@@ -37,6 +35,8 @@ mesh_convert <-
           res <- res %>%
             purrr::map(~ paste0(.x, seq_len(4))) %>%
             purrr::reduce(c)
+        if (to_mesh_size == units::as_units(0.100, "km"))
+          res <- make_100m_mesh(meshcode)
       } else {
         if (to_mesh_size == units::as_units(80, "km"))
           res <- substr(meshcode, 1, 4)
@@ -50,6 +50,8 @@ mesh_convert <-
           res <- substr(meshcode, 1, 10)
         if (to_mesh_size == units::as_units(0.125, "km"))
           res <- substr(meshcode, 1, 11)
+        if (to_mesh_size == units::as_units(0.100, "km"))
+          res <- substr(meshcode, 1, 10)
       }
       return(res)
 }
